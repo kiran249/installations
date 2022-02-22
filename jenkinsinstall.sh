5a01b7bbf0fe462f80cd8e677494d33007369021
kiran249 <60708138+kiran249@users.noreply.github.com>
2022-02-22 16:23:27 +0530

    Update jenkinsinstall.sh

--- jenkinsinstall.sh
+++ jenkinsinstall.sh
@@ -1,5 +1,5 @@
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-  sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo amazon-linux-extras install epel -y
 sudo yum install -y java-1.8.0-openjdk-devel
 sudo yum install jenkins -y
